f0b1694bbe3f74f8fbffcfc49b11bcc3780e8a07
Bnouhaila <nouhailabouchrit01@gmail.com>
2020-12-26 14:27:11 +0100

    a comment about your changes

--- TP1/ex04/cholesky.c
+++ TP1/ex04/cholesky.c
@@ -6,9 +6,10 @@
 int main()
 {
 	float A[N][N], B[N],X[N];
-	float C[N][N];
+	float C[N][N], CT[N][N];
 	int i,j,k,n;
 	float s1,s2;
+	float Y[N];
 	//the size of the matrix//
 	printf("donner la dimension de la matrice:");
 	scanf("%d",&n);
@@ -58,11 +59,51 @@ int main()
 	}
 
 	}
+
+	float m;
+	Y[0]=B[0]/C[0][0];
+	for(i=1; i<n;i++)
+	{
+		m=0;
+		for(j=0; j<i;j++)
+		{
+			m=m+C[i][j]*Y[j];
+		}
+	Y[i]=(B[i]-m)/C[i][i];	
+	}
+	printf("Y:\n");
 	for(i=0;i<n;i++)
 	{
-		for(j=0; j<n; j++)
-			printf("%f\n", C[i][j]);
+		printf("%lf\t", Y[i]);
+		printf("\n");
 	}
-		
+	float tr[N][N];
+	for(i=0;i<n; i++)
+	{
+		for(j=0;j<n;j++)
+			tr[i][j]=C[j][i];
+	}
+	printf("tr: \n");
+	for(i=0;i<n;i++)
+	{
+		for(j=0;j<n;j++)
+			printf("%lf\t", tr[i][j]);
+		printf("\n");
+	}
+
+	double m1;
+	X[n-1]=Y[n-1]/tr[n-1][n-1];
+	for(i=n-2;i>0;i--)
+	{
+		m1=0;
+		for( j=i+1;j<n;j++)
+		{
+			m1=m1+tr[i][j] * X[j];
+		}
+	X[i]=(Y[i]-m1)/tr[i][i];
+	}
+	printf("solution:");
+	for(i=0;i<n;i++)
+		printf("%lf\n", X[i]);
   return (0);
 }
